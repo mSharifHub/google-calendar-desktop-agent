11a5517348ebcca7a70f9828a2e370a8135994ab
mSharifHub <msharifemail@gmail.com>
2026-04-27 01:56:11 -0700

    implemented agent file

--- agent/agent.py
+++ agent/agent.py
@@ -1,5 +1,8 @@
+import datetime
+
 from langchain_ollama import ChatOllama
-from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langchain.agents import create_agent
 from tools.calendar_tools import create_event,edit_event, get_upcoming_events
 from tools.jobs_tools import view_job_tracker, scan_job_emails
 
@@ -13,4 +16,28 @@ tools = [
 ]
 
 
+current_date = datetime.datetime.now(datetime.timezone.utc).isoformat().replace('+00:00', 'Z')
 
+
+system_prompt = (
+        f"You are an agent whose job is to check users gmail for job applications and help user schedule, check, and update job application statuses and events."
+        f" Today's date/time is {current_date}. "
+        "You have full access to the user's Google Calendar and an internal Job Application Tracker. "
+        "Use the provided tools to check schedules, create meetings, and check or update job application statuses. "
+        "Always use get_upcoming_events to find an Event ID before attempting to use edit_event."
+    )
+
+
+memory = MemorySaver()
+
+model = ChatOllama(model="llama3.1", temperature=0)
+
+
+agent = create_agent(
+        model,
+        tools,
+        system_prompt = system_prompt,
+        checkpointer=memory
+    )
+
+config = {"configurable": {"thread_id": "terminal_session_1"}}
